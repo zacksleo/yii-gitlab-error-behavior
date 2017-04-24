b8edb61c718cca095c358c1c1183511fa3170265
zacksleo <zacksleo@gmail.com>
2017-04-24 14:27:16 +0800

    update report content

--- src/ErrorBehavior.php
+++ src/ErrorBehavior.php
@@ -26,7 +26,7 @@ class ErrorBehavior extends \CBehavior
     public function beforeAction($event)
     {
         $error = \Yii::app()->errorHandler->error;
-        if ($error > 499 && $error < 600) {
+        if ($error['code'] > 499 && $error['code'] < 600) {
             $projectId = $this->getProjectId();
             if (empty($projectId)) {
                 return true;
@@ -41,6 +41,7 @@ class ErrorBehavior extends \CBehavior
             if (!empty(\Yii::app()->request->getUserHostAddress())) {
                 $description .= '<blockquote>IP: ' . \Yii::app()->request->getUserHostAddress() . '</blockquote>';
             }
+            $description .= '<blockquote>URL: ' . \Yii::app()->request->url.'</blockquote>';
             if (!empty(\Yii::app()->request->getUrlReferrer())) {
                 $description .= '<blockquote>Refer:' . \Yii::app()->request->getUrlReferrer() . '</blockquote>';
             }
@@ -53,14 +54,15 @@ class ErrorBehavior extends \CBehavior
                 'UTF-8',
                 true
             );
+            $description .= '<blockquote>File: ' . $error['file'].'[Line:'.$error['line'].'][Type:'.$error['type'].']</blockquote>';
             $description .= '<blockquote>' . $content . '</blockquote>';
-            $description .= '<blockquote>URL: ' . \Yii::app()->request->url;
-            $description .= '</blockquote><br/><pre>' . $error['trace'] . '</pre>';
+            $description .= '<br/><pre>' . $error['trace'] . '</pre>';
+            $description .= '<br/><pre>' . $error['source'] . '</pre>';
             curl_setopt(
                 $ch,
                 CURLOPT_POSTFIELDS,
                 [
-                    'title' => $error['message'],
+                    'title' => '['.$error['code'].']'.$error['message'],
                     'description' => $description,
                     'labels' => 'bug',
                 ]
